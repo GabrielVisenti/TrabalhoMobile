e7dd7c14c8d441b3847956818d8123209351261b
GabrielVisenti <gabrielvisenti@gmail.com>
2026-03-03 22:34:50 -0300

    calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,145 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useRootNavigationState } from "expo-router";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Calculadora() {
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+  const [num1, setNum1] = useState ("");
+  const [num2, setNum2] = useState ("");
+  const [ valor, setValor] = useState ("");
+  const [cont, setCont] = useState (0);
+  const [resultado, setResultado] = useState (0);
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+  function adicionar(numero){
+    setValor(valor + numero)
+  }
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  function numero(somar){
+    const somar = valor;
+    const string = valor.toString();
+  }
+  
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  const somar = () => setCont(Number(valor) + Number(valor));
+
+  const diminuir = () => setCont (Number(valor) - Number(valor));
+
+  const multiplicar = () => setCont (Number(valor) * Number(valor));
+
+  const dividir = () => setCont (Number(valor) / Number(valor));
+
+  const raiz = () => setCont (Number(valor) * Number(valor));
+
+
+  return(
+
+    <SafeAreaView> 
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('0')}>
+        <Text> 0 </Text>
+      </TouchableOpacity>
+      </View> 
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('1')}>
+        <Text> 1 </Text>
+      </TouchableOpacity>
+      </View> 
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('2')}>
+        <Text> 2 </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('3')}>
+        <Text> 3 </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('4')}>
+        <Text> 4 </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('5')}>
+        <Text> 5 </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('6')}>
+        <Text> 6 </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('7')}>
+        <Text> 7 </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('8')}>
+        <Text> 8 </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+     <TouchableOpacity onPress={() => adicionar('9')}>
+        <Text> 9 </Text>
+      </TouchableOpacity>
+      </View>
+
+
+      <View>
+      <TouchableOpacity onPress={(somar) => adicionar('+')}>
+        <Text> + </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+      <TouchableOpacity onPress={(diminuir) => adicionar('-')}>
+        <Text> - </Text>
+      </TouchableOpacity>
+      </View>
+
+        <View>
+      <TouchableOpacity onPress={(multiplicar) => adicionar('*')}>
+        <Text> * </Text>
+      </TouchableOpacity>
+      </View>
+
+        <View>
+      <TouchableOpacity onPress={(dividir) => adicionar('/')}>
+        <Text> / </Text>
+      </TouchableOpacity>
+      </View>
+
+        <View>
+      <TouchableOpacity onPress={(raiz) => adicionar('r')}>
+        <Text> r </Text>
+      </TouchableOpacity>
+      </View>
+
+      <View>
+      <TouchableOpacity onPress={() => adicionar('=')}>
+        <Text> = </Text>
+      </TouchableOpacity>
+      </View>
+
+      <Text>Resultado: {valor} </Text>
+
+      
+    </SafeAreaView>
+  )
+
+
+
 }
+
+
